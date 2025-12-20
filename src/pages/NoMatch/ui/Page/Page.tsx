ee0bee6292a7e719f73e10b0f59fe64ad1637e10
Maybetuandat <trinhvinhtuandat05102003@gmail.com>
2025-12-20 12:54:52 +0700

    update ui lab  details

--- src/pages/NoMatch/ui/Page/Page.tsx
+++ src/pages/NoMatch/ui/Page/Page.tsx
@@ -1,20 +1,31 @@
-import { FC } from "react";
-import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
-const NoMatch: FC = () => {
+const Error = () => {
   return (
-    <section>
-      <div className="flex min-h-screen w-screen flex-col items-center justify-center gap-y-5">
-        <h1 className="bg-gradient-to-l from-primary-content via-secondary to-primary bg-clip-text text-9xl font-bold text-transparent">
-          404
-        </h1>
-        <p className="text-3xl font-medium text-neutral">Page not found</p>
-        <Link className="btn-primary-content btn px-16" to="/">
-          Go back
-        </Link>
+    <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
+      <div className="flex flex-col items-center justify-center px-4 py-8 text-center">
+        <h2 className="mb-6 text-5xl font-semibold">Whoops!</h2>
+        <h3 className="mb-1.5 text-3xl font-semibold">Something went wrong</h3>
+        <p className="text-muted-foreground mb-6 max-w-sm">
+          The page you&apos;re looking for isn&apos;t found, we suggest you back
+          to home.
+        </p>
+        <Button asChild size="lg" className="rounded-lg text-base">
+          <a href="#">Back to home page</a>
+        </Button>
       </div>
-    </section>
+
+      {/* Right Section: Illustration */}
+      <div className="relative max-h-screen w-full p-2 max-lg:hidden">
+        <div className="h-full w-full rounded-2xl bg-black"></div>
+        <img
+          src="https://cdn.shadcnstudio.com/ss-assets/blocks/marketing/error/image-1.png"
+          alt="404 illustration"
+          className="absolute left-1/2 top-1/2 h-[clamp(260px,25vw,406px)] -translate-x-1/2 -translate-y-1/2"
+        />
+      </div>
+    </div>
   );
 };
 
-export default NoMatch;
+export default Error;
